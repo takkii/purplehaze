afb98a1af92c5978467ff7d7e68a6217686c36b7
Ｔ・Ｋ <karuma.reason@gmail.com>
2026-06-30 11:51:50 +0900

    Update.

--- spec/models/string_spec.rb
+++ spec/models/string_spec.rb
@@ -1,27 +1,27 @@
-# rspec sample string matcher.
+# frozen_string_literal: true
 
 describe String do
-  it 'should be a kind of String' do
+  it 'should to be kind of String' do
     expect('hello').to be_kind_of(String)
   end
 
-  it 'should be a kind of String' do
+  it 'should to be a kind of String' do
     expect('hello').to be_a_kind_of(String)
   end
 
-  it 'should be a kind of String' do
+  it 'should to be a an instance of String' do
     expect('hello').to be_an_instance_of(String)
   end
 
-  it 'should be a kind of String' do
+  it 'should to be instance of String' do
     expect('hello').to be_instance_of(String)
   end
 
-  it 'should be a kind of String' do
+  it 'should to be String' do
     expect('hello').to be_a(String)
   end
 
-  it 'should be a kind of String' do
-    expect('hello').to be_an(String)
-  end
+  # it 'should to be an String' do
+  #   expect('hello').to be_an(String)
+  # end
 end
